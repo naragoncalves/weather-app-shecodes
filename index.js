8d041ae2083ccbfaa55ea974609368eaa026eb63
Nara <nara.jgs@gmail.com>
2023-02-23 17:01:57 +0100

    Added JS template

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ let date = currentTime.getDate();
 let h2 = document.querySelector("h2");
 h2.innerHTML = `${day} ${hours}:${minutes}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="40" />
+              <div> <span class="weather-forecast-temperatures"> 20° </span><span class="weather-forecast-temperature-max">18°</span><span class="weather-forecast-temperature-min">10°</span></div>
+          </div>
+
+`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function showTemperature(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temp").innerHTML = Math.round(
@@ -68,12 +90,6 @@ function getLocation(position) {
   axios.get(apiUrl).then(showTemperature);
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", citySearch);
-
-let currentLocation = document.querySelector("#location-button");
-currentLocation.addEventListener("click", getCurrentPosition);
-
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
@@ -94,3 +110,11 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", citySearch);
+
+let currentLocation = document.querySelector("#location-button");
+currentLocation.addEventListener("click", getCurrentPosition);
+
+displayForecast();
