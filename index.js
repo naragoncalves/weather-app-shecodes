c6de08d889d50fc19e0be72adae05918e94c0f62
Nara <nara.jgs@gmail.com>
2023-02-26 09:52:56 +0100

    Added API to display forecast data

--- index.js
+++ index.js
@@ -47,6 +47,12 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temp").innerHTML = Math.round(
@@ -67,6 +73,8 @@ function showTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0]);
 
   celsiusTemperature = Math.round(response.data.main.temp);
+
+  getForecast(response.data.coord);
 }
 
 function citySearch(event) {
