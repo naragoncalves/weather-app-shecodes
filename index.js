6f55dfa262e7b2dbe1372b92a0c4481839581480
Nara <nara.jgs@gmail.com>
2023-02-26 11:19:36 +0100

    Updated css style

--- index.js
+++ index.js
@@ -94,6 +94,12 @@ function showTemperature(response) {
   getForecast(response.data.coord);
 }
 
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
 function citySearch(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
@@ -142,4 +148,4 @@ searchForm.addEventListener("submit", citySearch);
 let currentLocation = document.querySelector("#location-button");
 currentLocation.addEventListener("click", getCurrentPosition);
 
-displayForecast();
+search("Amsterdam");
